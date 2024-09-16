d8687e3897835fb74f9754258586b27c59214538
muzabol2 <bolekbilly@gmail.com>
2024-09-16 19:25:35 +0200

    Change shapes color contrast

--- src/constants/shapes.ts
+++ src/constants/shapes.ts
@@ -1,46 +1,46 @@
 const SHAPES: Record<string, { shape: number[][]; color: string }> = {
-  I: { shape: [[1, 1, 1, 1]], color: "#00FFFF" }, // Cyan
+  I: { shape: [[1, 1, 1, 1]], color: "#7FDBFF" }, // Soft Cyan
   J: {
     shape: [
       [0, 0, 1],
       [1, 1, 1],
     ],
-    color: "#0000FF", // Blue
+    color: "#0074D9", // Soft Blue
   },
   L: {
     shape: [
       [1, 0, 0],
       [1, 1, 1],
     ],
-    color: "#FFA500", // Orange
+    color: "#FF851B", // Soft Orange
   },
   O: {
     shape: [
       [1, 1],
       [1, 1],
     ],
-    color: "#FFFF00", // Yellow
+    color: "#FFDC00", // Soft Yellow
   },
   S: {
     shape: [
       [0, 1, 1],
       [1, 1, 0],
     ],
-    color: "#00FF00", // Green
+    color: "#2ECC40", // Soft Green
   },
   T: {
     shape: [
       [0, 1, 0],
       [1, 1, 1],
     ],
-    color: "#800080", // Purple
+    color: "#B10DC9", // Soft Purple
   },
   Z: {
     shape: [
       [1, 1, 0],
       [0, 1, 1],
     ],
-    color: "#FF0000", // Red
+    color: "#FF4136", // Soft Red
   },
 };
 
