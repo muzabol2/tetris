ae3f00deccc15e75276bbfd21d5fc26fb964a271
muzabol2 <bolekbilly@gmail.com>
2024-09-26 22:33:45 +0100

    Add LoadingSpinner

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { ErrorBoundary } from "./ErrorBoundary";
 export { GameBoard } from "./GameBoard";
 export { GameMenu } from "./GameMenu";
+export { LoadingSpinner } from "./LoadingSpinner";
 export { MobileButtons } from "./MobileButtons";
 export { Navbar } from "./Navbar";
--- src/context/TetrisProvider.tsx
+++ src/context/TetrisProvider.tsx
@@ -2,13 +2,14 @@
 
 import { TetrisContext } from "./TetrisContext";
 import { useTetrisActions } from "./actions/useTetrisActions";
+import { LoadingSpinner } from "@/components";
 import React from "react";
 
 const TetrisProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { state, dispatch, isLoaded } = useTetrisActions();
 
   if (!isLoaded) {
-    return <div>Loading...</div>; // TODO: Add a loading spinner
+    return <LoadingSpinner />;
   }
 
   return <TetrisContext.Provider value={{ state, dispatch }}>{children}</TetrisContext.Provider>;
